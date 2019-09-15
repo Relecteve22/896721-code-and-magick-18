e4ef348e9a8ce11029e87b0e00a0309f02bde946
Relecteve <Filonovaval@gmail.com>
2019-09-15 20:49:18 +0300

    сделал половину задания

---
+++ js/stat.js
@@ -0,0 +1,24 @@
+'use strict';
+
+window.renderStatistics = function (ctx, names, times) {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(110, 20, 420, 270);
+
+  ctx.fillStyle = 'white';
+  ctx.fillRect(100, 10, 420, 270);
+
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+  ctx.textBaseline = 'hanging';
+  ctx.fillText('Ура вы победили!', 120, 35);
+  ctx.fillText('Список результатов:', 120, 55);
+
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+  ctx.textBaseline = 'hanging';
+  ctx.fillText('Ура вы победили!', 120, 105);
+
+  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+  ctx.fillRect(150, 100, 40, 150);
+
+};
