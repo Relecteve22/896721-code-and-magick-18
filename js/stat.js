70bcf0a2b3306d9f70df0ab1fff56282de530b19
Relecteve <Filonovaval@gmail.com>
2019-09-15 21:34:11 +0300

    не доделал половину задания

--- js/stat.js
+++ js/stat.js
@@ -1,11 +1,16 @@
 'use strict';
 
-window.renderStatistics = function (ctx, names, times) {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(110, 20, 420, 270);
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
 
-  ctx.fillStyle = 'white';
-  ctx.fillRect(100, 10, 420, 270);
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+window.renderStatistics = function (ctx, names, times) {
+  renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, 100, 10, 'white');
 
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
@@ -13,12 +18,6 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Ура вы победили!', 120, 35);
   ctx.fillText('Список результатов:', 120, 55);
 
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
-  ctx.textBaseline = 'hanging';
-  ctx.fillText('Ура вы победили!', 120, 105);
-
   ctx.fillStyle = 'rgba(255, 0, 0, 1)';
   ctx.fillRect(150, 100, 40, 150);
-
 };
