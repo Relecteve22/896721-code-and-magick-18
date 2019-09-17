34342a474b4c643654d0528ff3248d9a14a97b83
Relecteve <Filonovaval@gmail.com>
2019-09-17 08:29:51 +0300

    делаю задание

--- js/stat.js
+++ js/stat.js
@@ -2,22 +2,43 @@
 
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
+var COORDINATE_X_CLOUD = 100;
+var COORDINATE_Y_CLOUD = 10;
+var COLOR_CLOUD = 'white';
+var COLOR_SHADEOW = 'rgba(0, 0, 0, 0.7)';
+var GAP_CLOUD = 10;
+var TEXT_FONT = '16px PT Mono';
+var COLOR_TEXT = '#000';
 
-var renderCloud = function (ctx, x, y, color) {
+var drawCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, 100, 10, 'white');
-
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
+var drawText = function (ctx, text, coordinateX, coordinateY) {
+  ctx.fillStyle = COLOR_TEXT;
+  ctx.font = TEXT_FONT;
   ctx.textBaseline = 'hanging';
-  ctx.fillText('Ура вы победили!', 120, 35);
-  ctx.fillText('Список результатов:', 120, 55);
+  ctx.fillText(text, coordinateX, coordinateY);
+};
 
-  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  ctx.fillRect(150, 100, 40, 150);
+window.renderStatistics = function (ctx, names, times) {
+  drawCloud(ctx, COORDINATE_X_CLOUD + GAP_CLOUD, COORDINATE_Y_CLOUD + GAP_CLOUD, COLOR_SHADEOW);
+  drawCloud(ctx, COORDINATE_X_CLOUD, COORDINATE_Y_CLOUD, COLOR_CLOUD);
+
+  drawText(ctx, 'Ура вы победили!', 120, 35);
+  drawText(ctx, 'Список результатов:', 120, 55);
+
+  // ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+  // ctx.fillRect(150, 100, 40, 150);
+
+  var drawColumn = function (arrayNames, arrayTimes) {
+    var coordX = 140;
+    for (var i = 0; i < arrayNames.length; i++) {
+      drawText(ctx, arrayNames[i], coordX, 250);
+      coordX = coordX + 100;
+    }
+  };
+
+  drawColumn(names);
 };
